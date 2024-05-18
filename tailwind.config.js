6ddd36c8795e5bcfbe6a2d931c9ea2ea931ab2c3
Eladio Caritos <eladio@caritos.com>
2024-05-18 10:01:33 -0400

    updating tailwind config file for custom elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.ftl", "./resources/templates/*.ftl", "./resources/templates/**/*.ftl"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      }
+    },
   },
+  variants: {},
   plugins: [],
 }
