0eb8bdb8c27f5ea0e7887004104a46f281296fc3
Eladio Caritos <eladio@caritos.com>
2024-05-23 16:52:17 -0400

    cleaning up the code.. removing unused import statement.

--- src/main/kotlin/com/caritos/Application.kt
+++ src/main/kotlin/com/caritos/Application.kt
@@ -1,16 +1,13 @@
 package com.caritos
 
-import com.caritos.dao.DatabaseSingleton
 import com.caritos.plugins.*
 import configureDatabase
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
