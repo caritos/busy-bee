6f4ebad4421dd944196a366e9ec26d09d6366b56
Eladio Caritos <eladio@caritos.com>
2024-05-27 08:32:50 -0400

    changed from h2 to postgres

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val exposed_version: String by project
-val h2_version: String by project
+val postgresql_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.24"
@@ -37,8 +37,8 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("org.postgresql:postgresql:$postgresql_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/com/caritos/dao/DatabaseSingleton.kt
+++ src/main/kotlin/com/caritos/dao/DatabaseSingleton.kt
@@ -8,9 +8,11 @@ import org.jetbrains.exposed.sql.transactions.experimental.*
 
 object DatabaseSingleton {
     fun init() {
-        val driverClassName = "org.h2.Driver"
-        val jdbcURL = "jdbc:h2:file:./build/db"
-        val database = Database.connect(jdbcURL, driverClassName)
+        val driverClassName = "org.postgresql.Driver"
+        val jdbcURL = "jdbc:postgresql://localhost:5432/tennis"
+        val username = "user_a"
+        val password = "password_a"
+        val database = Database.connect(jdbcURL, driverClassName, username, password)
         transaction(database) {
             SchemaUtils.create(Users)
             SchemaUtils.create(Articles)
