dd1703b41f3538371a35ecc82ca3bb0b30e64f17
Eladio Caritos <eladio@caritos.com>
2024-07-06 04:06:44 -0400

    to deploy to digital ocean, i have to change the value of the host.

--- src/main/kotlin/com/caritos/Application.kt
+++ src/main/kotlin/com/caritos/Application.kt
@@ -8,7 +8,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "localhost", module = Application::module).start(wait = true)
+    embeddedServer(Netty, port = 80, host = "0.0.0.0", module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
